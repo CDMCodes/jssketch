71df50a61cbc9b1453e0cd134f54a10477083aba
Andrew Campbell <campbell.drew.m@gmail.com>
2017-05-28 14:55:11 -0400

    Create initial pixels on canvas
    Create two JS loops to add 16 rows of 16 pixels to canvas.  Add styling
    for pixel row and pixels to CSS sheet.  Note that styling is incomplete,
    as pixels do not fill the canvas.

--- draw.js
+++ draw.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-
+  var pixel = '<div class="pixel"></div>'
+  var pixRow = '<div class="pixRow"></div>'
+  var gridSize = 16;
+  for(i=1;i<=gridSize;i++){
+      $(".canvas").append(pixRow);
+  }
+  for(i=1;i<=gridSize;i++){
+      $(".pixRow").append(pixel);
+    }
 });
