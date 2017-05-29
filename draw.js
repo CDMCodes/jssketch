f2f003794928103305d595d0b4118b1a940dbe57
Andrew Campbell <campbell.drew.m@gmail.com>
2017-05-29 13:20:17 -0400

    Set 'clear' and 'new' button functions
    Add id's to clear and new buttons.  Modify CSS to make pixel borders
    visible for testing.  Define pixel drawing and color effect within
    a function, allowing 'new canvas' button to call said function with
    a size argument.

--- draw.js
+++ draw.js
@@ -2,24 +2,42 @@ $(document).ready(function(){
   //Define html for pixel and pixel row that will be appended
   var pixel = '<div class="pixel"></div>'
   var pixRow = '<div class="pixRow"></div>'
-  //Definte variable to hold grid size, and the height and width fractions
+  //Define variable to hold grid size, and the height and width fractions
   var gridSize = 16;
-  var size = (1/16) *100;
+  var initial = (1/16) *100;
 
-  //First Loop: add the requisite number of rows and assign row heights
-  for(i=1;i<=gridSize;i++){
-      $(".canvas").append(pixRow);
-      $(".pixRow").css({"height": size + "%"})
-  }
-
-  //Second Loop: add the requisite number of pixels and assign widths
-  for(i=1;i<=gridSize;i++){
-      $(".pixRow").append(pixel);
-      $(".pixel").css({"width":size + "%"})
+  function drawGrid(size){
+    //First Loop: add the requisite number of rows and assign row heights
+    for(i=1;i<=gridSize;i++){
+        $(".canvas").append(pixRow);
+        $(".pixRow").css({"height": size + "%"})
     }
-    //console.log(size);
 
+    //Second Loop: add the requisite number of pixels and assign widths
+    for(i=1;i<=gridSize;i++){
+        $(".pixRow").append(pixel);
+        $(".pixel").css({"width":size + "%"})
+      }
+
+    //The 'draw' effect - Change background color on mouse enter
     $('.pixel').on('mouseenter',function(){
       $(this).css("background-color","black");
     })
+  }
+
+  drawGrid(initial);
+
+  //Clear canvas button
+  $('#clear').on('click',function(){
+    $('.pixel').css("background-color","white");
+  })
+
+  //New canvas button
+  $('#new').on('click',function(){
+    var newSize = prompt("How many pixels per row? (canvas is square)?");
+    newSize = (1/newSize)*100;
+    drawGrid(newSize);
+    //console.log(newSize);
+  })
+
 });
