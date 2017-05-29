19fbb6da8e5073c7e930686f54e43390f1463377
Andrew Campbell <campbell.drew.m@gmail.com>
2017-05-29 13:00:31 -0400

    Correct canvas and pixel size
    Modify CSS to remove testing borders and sizes.  Correct JS file
    to correctly calculate the width and height for a fixed value, in
    this case 16.

--- draw.js
+++ draw.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
+  //Define html for pixel and pixel row that will be appended
   var pixel = '<div class="pixel"></div>'
   var pixRow = '<div class="pixRow"></div>'
+  //Definte variable to hold grid size, and the height and width fractions
   var gridSize = 16;
+  var size = (1/16) *100;
+
+  //First Loop: add the requisite number of rows and assign row heights
   for(i=1;i<=gridSize;i++){
       $(".canvas").append(pixRow);
+      $(".pixRow").css({"height": size + "%"})
   }
+
+  //Second Loop: add the requisite number of pixels and assign widths
   for(i=1;i<=gridSize;i++){
       $(".pixRow").append(pixel);
+      $(".pixel").css({"width":size + "%"})
     }
+    //console.log(size);
 });
